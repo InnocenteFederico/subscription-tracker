95abb1b2be14194282d2709f6b1fa465030889a4
Federico Innocente <innocente.federico.97@gmail.com>
2026-06-16 14:13:05 +0200

    chore: setup springboot

---
+++ src/main/java/com/federicoinnocente/subs_tracker/SubsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.federicoinnocente.subs_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/federicoinnocente/subs_tracker/SubsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.federicoinnocente.subs_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
